f98340c205cb57159ac383023a4b9be6f662c9a2
Winston Tamin <winstontamin@gmail.com>
2024-10-22 09:12:32 +0700

    second commit

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,6 +2,7 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { useEffect } from 'react';
 import Header from '../utilities/Header';
+import { Link } from 'react-router-dom'
 
 const HomePage = () => {
 
@@ -9,6 +10,8 @@ const HomePage = () => {
     useEffect(() => {
         AOS.init({ duration: 2000 });
     }, []);
+
+
 
 
     return (
@@ -19,6 +22,9 @@ const HomePage = () => {
                 <div className="flex flex-col gap-3 justify-center items-center">
                     <p data-aos="fade-in" data-aos-once="true" className="text-white font-bold text-5xl">From Our Kitchen to Your Table.</p>
                     <p data-aos="fade-in" data-aos-once="true" data-aos-delay="1000" data-aos-duration="1000" className="tracking-[5px] text-[12px] text-white/50 font-normal uppercase">uncle wu's <span className="text-yellow-100">delights!</span></p>
+                    <Link data-aos="fade-in" data-aos-once="true" data-aos-delay="1000" data-aos-duration="1000" to={'/menu'} className='animate-float hover:bg-yellow-100/40 bg-yellow-100/30 border-none border outline-none block text-yellow-100 font-normal py-2 px-5 mt-5'>
+                        View Menu
+                    </Link>
 
                 </div>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,15 @@ export default {
   ],
   theme: {
     extend: {
+      animation: {
+        float: 'float 3s ease-in-out infinite',
+      },
+      keyframes: {
+        float: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-10px)' },
+        },
+      },
       backgroundImage: {
         'homebg': "url('./assets/image/nasgor.png')",
       },
